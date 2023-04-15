077be0cbada527f1d01c7ff5e5528e04ef3109f2
Pavel Chmykh <pchmykh@outlook.com>
2023-04-15 21:36:56 +0300

    yac

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM php:8.2.5RC1-apache-bullseye
+
+COPY index1.php /var/www/html/
+
+EXPOSE 80
+
---
+++ index1.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Current Date and Time</title>
+  </head>
+  <body>
+    <h1>Current DevOps Date and Time</h1>
+    <p>
+      <?php echo "The current DevOps date and time is: " . date("Y-m-d H:i:s"); ?>
+    </p>
+  </body>
+</html>
